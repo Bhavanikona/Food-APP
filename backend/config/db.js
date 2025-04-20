ca2ce8bd5b1b9c10154282bd9fa8f9477020082d
Bhavanikona <bhanukona24@gmail.com>
2025-04-20 10:08:39 +0530

    new changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,10 +9,9 @@ export const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/food-app';
 
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI
+      
+    );
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
